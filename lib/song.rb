282ef70fda40751390c99c9ae78a0758af300a6e
reduviidae <wiljago@gmail.com>
2018-12-13 21:31:32 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require "pry"
+
+class Song
+
+attr_accessor :name, :artist, :genre
+
+@@count = 0
+@@artists = []
+@@genres = []
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+
+  @@count += 1
+  @@artists << self.artist
+  @@genres << self.genre
+end
+
+def count
+end
+
+end
