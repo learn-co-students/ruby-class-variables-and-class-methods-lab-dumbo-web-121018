4a97e6aea0410932e7ff18fa99186eefa18c6789
reduviidae <wiljago@gmail.com>
2018-12-16 15:19:05 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,53 @@ def initialize(name, artist, genre)
   @@genres << self.genre
 end
 
-def count
-  binding.pry
+# binding.pry
+
+def self.count
+  # binding.pry
+  @@count
+end
+
+def self.artists
+  @@artists.uniq
+end
+
+def self.genres
+  @@genres.uniq
+  # binding.pry
+end
+
+def self.genre_count
+  # binding.pry
+  temp = ""
+  genre_hash = {}
+  @@genres.each do |genre|
+    #binding.pry
+    if genre != temp
+      genre_hash[genre] = 1
+      temp = genre
+    else
+      genre_hash[genre] += 1
+      temp = genre
+    end
+  end
+  genre_hash
+end
+
+def self.artist_count
+  temp = ""
+  artist_hash = {}
+  @@artists.each do |artist|
+    # binding.pry
+    if artist != temp
+      artist_hash[artist] = 1
+      temp = artist
+    else
+      artist_hash[artist] += 1
+      temp = artist
+    end
+  end
+  artist_hash
 end
 
 end
